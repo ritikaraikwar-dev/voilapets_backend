3f9efd4f1f7890e70d1edeb8526fdc1721155827
ritika_dev <ritikaraikwar24@gmail.com>
2025-10-22 19:12:15 +0530

    server

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 
 const dbConnect = async () => {
   try {
+    console.log('-----------------------------------------------connect to bd before');
+    
     // const db = await mongoose.connect("mongodb://localhost:27017/voilapets",);
    const db = await mongoose.connect("mongodb+srv://ritikaitgeeks_db_user:itgeeks%40123@voilapetsdb.tsoquha.mongodb.net/?retryWrites=true&w=majority&appName=voilapetsdb")
+    console.log('-----------------------------------------------connect to bd after');
 
     console.log(`database connected successfully ${db.connection.name}`);
   } catch (error) {
