9ba369dc801027d2564e1c6d8e2ca5d337dbfb3e
ritika_dev <ritikaraikwar24@gmail.com>
2025-10-22 19:18:19 +0530

    add env

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-
+require('dotenv').config();
 const dbConnect = async () => {
   try {
     console.log('-----------------------------------------------connect to bd before');
     
     // const db = await mongoose.connect("mongodb://localhost:27017/voilapets",);
-   const db = await mongoose.connect("mongodb+srv://ritikaitgeeks_db_user:itgeeks%40123@voilapetsdb.tsoquha.mongodb.net/?retryWrites=true&w=majority&appName=voilapetsdb")
+   const db = await mongoose.connect(process.env.MONGO_URI)
     console.log('-----------------------------------------------connect to bd after');
 
     console.log(`database connected successfully ${db.connection.name}`);
