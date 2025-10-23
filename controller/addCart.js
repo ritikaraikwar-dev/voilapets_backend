2ad2afac784cf7bf7e2ce713039956afd13e8e37
ritika_dev <ritikaraikwar24@gmail.com>
2025-10-23 11:30:06 +0530

    add to cart

--- controller/addCart.js
+++ controller/addCart.js
@@ -1,30 +1,70 @@
-const Cart = require('../models/Cart');
+// const Cart = require('../models/Cart');
 
-// api for add to cart 
+// const addCart = async(req, res)=>{
+//     try{
+//     const sessionId = req.sessionID;
 
-const addCart = async(req, res)=>{
-    try{
-    const sessionId = req.sessionID;
+//         const{id, title, price, image, color} =   req.body;
 
-        const{id, title, price, image, color} =   req.body;
-
-         if (!sessionId) {
-      return res.status(400).json({ message: "Session ID missing" });
-    }
+//          if (!sessionId) {
+//       return res.status(400).json({ message: "Session ID missing" });
+//     }
              
-        const Cart = await Cart.create({id, title, price, image, color , session_id:sessionId});
+//         const Cart = await Cart.create({id, title, price, image, color , session_id:sessionId});
 
-        res.status(201).json({
-            message:"data send successfully",
-            userData:Cart
-        })
-    }
-    catch(error){
-          res.status(500).json({
-            message:error.message
+//         res.status(201).json({
+//             message:"data send successfully",
+//             userData:Cart
+//         })
+//     }
+//     catch(error){
+//           res.status(500).json({
+//             message:error.message
             
-          })
+//           })
+//     }
+// }
+
+// module.exports = addCart;
+
+
+const Cart = require('../models/Cart');
+const User = require('../models/User');
+
+const addCart = async (req, res) => {
+  try {
+    const { userId, productId, title, price, image, color, sessionId } = req.body;
+
+    if (!userId) {
+      return res.status(400).json({ message: "userId is required" });
     }
-}
+
+    // Check if user exists
+    const userExists = await User.findById(userId);
+    if (!userExists) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Create new cart item
+    const newCartItem = await Cart.create({
+      userId,
+      productId,
+      title,
+      price,
+      image,
+      color,
+      sessionId
+    });
+
+    res.status(201).json({
+      message: "Item added to cart successfully",
+      cartItem: newCartItem
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+};
 
 module.exports = addCart;
