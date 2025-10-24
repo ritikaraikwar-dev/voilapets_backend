d3aa3b2e13e2039b39298cf2872ebe8ab4748b4e
ritika_dev <ritikaraikwar24@gmail.com>
2025-10-24 15:48:36 +0530

    .

--- controller/getCart.js
+++ controller/getCart.js
@@ -8,17 +8,17 @@ const getCart = async(req , res)=>{
        const guestId = req.params.id;
     if (!guestId) return res.status(400).json({ message: 'Guest ID missing' });
 
-         const products = await User.find({ guest_id: guestId }); // make sure you use Cart model here
+        //  const products = await User.find({ guest_id: guestId }); // make sure you use Cart model here
           
-          if(!products){
-           return res.status(300).json({
-                message:"product is not available in cart"
-            })
-          }
+        //   if(!products){
+        //    return res.status(300).json({
+        //         message:"product is not available in cart"
+        //     })
+        //   }
 
         return  res.status(200).json({
             message:"data get successfully",
-            product:products
+            
           })
      }
      catch(error){
