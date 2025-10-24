2dac65b4b24ad8d49429b9efe3bd0e610d749878
ritika_dev <ritikaraikwar24@gmail.com>
2025-10-24 15:45:24 +0530

    .

--- controller/addCart.js
+++ controller/addCart.js
@@ -1,33 +1,3 @@
-// const Cart = require('../models/Cart');
-
-// const addCart = async(req, res)=>{
-//     try{
-//     const sessionId = req.sessionID;
-
-//         const{id, title, price, image, color} =   req.body;
-
-//          if (!sessionId) {
-//       return res.status(400).json({ message: "Session ID missing" });
-//     }
-             
-//         const Cart = await Cart.create({id, title, price, image, color , session_id:sessionId});
-
-//         res.status(201).json({
-//             message:"data send successfully",
-//             userData:Cart
-//         })
-//     }
-//     catch(error){
-//           res.status(500).json({
-//             message:error.message
-            
-//           })
-//     }
-// }
-
-// module.exports = addCart;
-
-
 const Cart = require('../models/Cart');
 
 const addCart = async (req, res) => {
